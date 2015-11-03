4af176c53505a14f065f1dc8a2f8d7da3a6d38b1
Felix Rieseberg <felix@felixrieseberg.com>
2015-11-03 10:37:58 -0800

    Upgrade Node and NPM
    - Will now use Node 0.12 and npm 3.3.x
    - Closes #13

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -46,8 +46,8 @@ SCRIPT_DIR="${BASH_SOURCE[0]%\\*}"
 SCRIPT_DIR="${SCRIPT_DIR%/*}"
 ARTIFACTS=$SCRIPT_DIR/../artifacts
 KUDU_SYNC_CMD=${KUDU_SYNC_CMD//\"}
-NODE_EXE="$PROGRAMFILES\\nodejs\\0.10.32\\node.exe"
-NPM_CMD="\"$NODE_EXE\" \"$PROGRAMFILES\\npm\\3.1.0\\node_modules\\npm\\bin\\npm-cli.js\""
+NODE_EXE="$PROGRAMFILES\\nodejs\\0.12.6\\node.exe"
+NPM_CMD="\"$NODE_EXE\" \"$PROGRAMFILES\\npm\\3.3.9\\node_modules\\npm\\bin\\npm-cli.js\""
 NODE_MODULES_DIR="$APPDATA\\npm\\node_modules"
 
 EMBER_PATH="$NODE_MODULES_DIR\\ember-cli\\bin\\ember"
